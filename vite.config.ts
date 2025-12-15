c172a6e58bb8569ca9a88fdea114793aeea25529
sardorphc <45298350+sardorphc@users.noreply.github.com>
2025-12-15 22:40:56 +0500

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      src: '/src'
+      'src': path.resolve(__dirname, './src')
     }
   }
 })
